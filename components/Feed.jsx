7317b0d846378b698d20572581ab4a6345e26d6f
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-10 16:30:14 +0530

    removed handle tagclick

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,14 +4,14 @@ import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
 
-const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data}) => {
   return (
     <div className='mt-16 prompt_layout'>
       {data.map((post) => (
         <PromptCard
           key={post._id}
           post={post}
-          handleTagClick={handleTagClick}
+          // handleTagClick={handleTagClick}
         />
       ))}
     </div>
