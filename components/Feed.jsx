67088a040a1332e5f12dbc84e7e192c1bca81727
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-10 16:27:55 +0530

    removed input

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -71,7 +71,7 @@ const Feed = () => {
 
   return (
     <section className='feed'>
-      <form className='relative w-full flex-center'>
+      {/* <form className='relative w-full flex-center'>
         <input
           type='text'
           placeholder='Search for a tag or a username'
@@ -80,7 +80,7 @@ const Feed = () => {
           required
           className='search_input peer'
         />
-      </form>
+      </form> */}
 
       {/* All Prompts */}
       {/* {searchText ? (
