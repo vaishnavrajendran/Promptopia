cf52fb02002024f369824176e337aea732e13890
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-11 08:24:57 +0530

    pro

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -17,7 +17,8 @@ const UpdatePrompt = () => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
+      console.log("Pro Response", response)
+      console.log("Pro Data", data)
       setPost({
         prompt: data.prompt,
         tag: data.tag,
