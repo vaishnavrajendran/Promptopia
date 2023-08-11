f8753ac1632b64aeae7c048be7738492b05d0955
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-11 07:50:39 +0530

    removed ?

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,7 @@ import Prompt from "@models/prompt";
 export const GET = async () => {
    try {
      await connectToDB();
-     const prompts = await Prompt.find({})?.populate('creator');
+     const prompts = await Prompt.find({}).populate('creator');
      console.log("Prompts>>", prompts);
      return new Response(JSON.stringify(prompts), { status:200 })
    } catch (error) {
