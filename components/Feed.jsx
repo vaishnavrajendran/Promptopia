aced02dbbc235a0bf57a49205382838b7ae8b5b4
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-11 06:37:09 +0530

    removed comments

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -11,7 +11,6 @@ const PromptCardList = ({ data}) => {
         <PromptCard
           key={post._id}
           post={post}
-          // handleTagClick={handleTagClick}
         />
       ))}
     </div>
@@ -20,11 +19,6 @@ const PromptCardList = ({ data}) => {
 
 const Feed = () => {
   const [allPosts, setAllPosts] = useState([]);
-
-  // Search states
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
-  const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
@@ -37,60 +31,9 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
-  // const filterPrompts = (searchtext) => {
-  //   const regex = new RegExp(searchtext, "i");
-  //   filteredPosts =  allPosts.filter(
-  //     (item) =>
-  //       regex.test(item.creator.username) ||
-  //       regex.test(item.tag) ||
-  //       regex.test(item.prompt)
-  //   );
-  //   console.log("returned Posts", filteredPosts);
-  //   return filteredPosts;
-  // };
-
-  // const handleSearchChange = (e) => {
-  //   clearTimeout(searchTimeout);
-  //   setSearchText(e.target.value);
-
-  //   // debounce method
-  //   setSearchTimeout(
-  //     setTimeout(() => {
-  //       const searchResult = filterPrompts(e.target.value);
-  //       setSearchedResults(searchResult);
-  //     }, 500)
-  //   );
-  // };
-
-  // const handleTagClick = (tagName) => {
-  //   setSearchText(tagName);
-
-  //   const searchResult = filterPrompts(tagName);
-  //   setSearchedResults(searchResult);
-  // };
-
   return (
     <section className='feed'>
-      {/* <form className='relative w-full flex-center'>
-        <input
-          type='text'
-          placeholder='Search for a tag or a username'
-          value={searchText}
-          onChange={handleSearchChange}
-          required
-          className='search_input peer'
-        />
-      </form> */}
-
-      {/* All Prompts */}
-      {/* {searchText ? (
-        <PromptCardList
-          data={searchedResults}
-          handleTagClick={handleTagClick}
-        />
-      ) : ( */}
         <PromptCardList data={allPosts} />
-      {/* )} */}
     </section>
   );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -68,7 +68,6 @@ const PromptCard = ({ post, handleEdit, handleDelete }) => {
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
       <p
         className='font-inter text-sm blue_gradient cursor-pointer'
-        // onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         #{post.tag}
       </p>
